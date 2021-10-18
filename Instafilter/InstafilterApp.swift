bab21f8d84eced2b0745de3fc00b6ae6fe699f5e
ALeX <alexdeoliveira.poli@gmail.com>
2021-10-18 08:28:22 -0300

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Alex Oliveira on 18/10/21.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
