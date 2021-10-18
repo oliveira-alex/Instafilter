7a1d2bc70bcc02f701574e1e3347efe2e11ddbe7
ALeX <alexdeoliveira.poli@gmail.com>
2021-10-18 10:15:50 -0300

    Integrating Core Image with SwiftUI – Instafilter SwiftUI Tutorial 4/12

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -5,27 +5,44 @@
 //  Created by Alex Oliveira on 18/10/21.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingActionSheet = false
-    @State private var backgroundColor = Color.white
+    @State private var image: Image?
     
     var body: some View {
-        Text("Hello, world!")
-            .frame(width: 300, height: 300)
-            .background(backgroundColor)
-            .onTapGesture {
-                self.showingActionSheet = true
-            }
-            .actionSheet(isPresented: $showingActionSheet) {
-                ActionSheet(title: Text("Change background"), message: Text("Select a new color"), buttons: [
-                    .default(Text("Red")) { self.backgroundColor = .red },
-                    .default(Text("Green")) { self.backgroundColor = .green },
-                    .default(Text("Blue")) { self.backgroundColor = .blue },
-                    .cancel()
-                ])
-            }
+        VStack {
+            image?
+                .resizable()
+                .scaledToFit()
+        }
+        .onAppear(perform: loadImage)
+    }
+    
+    func loadImage() {
+        guard let inputImage = UIImage(named: "Example") else { return }
+        let beginImage = CIImage(image: inputImage)
+        
+        let context = CIContext()
+//        let currentFilter = CIFilter.sepiaTone()
+//        let currentFilter = CIFilter.crystallize()
+        guard let currentFilter = CIFilter(name: "CITwirlDistortion") else { return }
+        
+//        currentFilter.intensity = 1 // for sepiaTone
+//        currentFilter.inputImage = beginImage  // new way of set inputImage (doesn't work for all kinds of filters)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey) // old way of set inputImage
+//        currentFilter.radius = 200 // for crystallize
+        currentFilter.setValue(2000, forKey: kCIInputRadiusKey)
+        currentFilter.setValue(CIVector(x: inputImage.size.width / 2, y: inputImage.size.height / 2), forKey: kCIInputCenterKey)
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+        
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgimg)
+            image = Image(uiImage: uiImage)
+        }
     }
 }
 
