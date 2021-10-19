3b58c76a34396d5a9252769c4118f7dd685f89b1
ALeX <alexdeoliveira.poli@gmail.com>
2021-10-19 07:07:22 -0300

    Basic image filtering using Core Image – Instafilter SwiftUI Tutorial 10/12

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -5,17 +5,32 @@
 //  Created by Alex Oliveira on 18/10/21.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
     @State private var image: Image?
-    @State private var filterIntensity = 0.5
+    @State private var filterIntensity = 0.0
     
     @State private var showingImagePicker = false
     @State private var inputImage: UIImage?
     
+    @State var currentFilter = CIFilter.sepiaTone()
+    let context = CIContext()
+    
     var body: some View {
-        NavigationView {
+        let intensity = Binding<Double>(
+            get:{
+                self.filterIntensity
+            },
+            set: {
+                self.filterIntensity = $0
+                self.applyProcessing()
+            }
+        )
+        
+        return NavigationView {
             VStack {
                 ZStack {
                     Rectangle()
@@ -37,7 +52,8 @@ struct ContentView: View {
                 
                 HStack {
                     Text("Intensity")
-                    Slider(value: self.$filterIntensity)
+                    Slider(value: intensity)
+                        .disabled(image == nil ? true : false)
                 }
                 .padding(.vertical)
                 
@@ -51,6 +67,7 @@ struct ContentView: View {
                     Button("Save") {
                         // save the picture
                     }
+                    .disabled(image == nil ? true : false)
                 }
             }
             .padding([.horizontal, .bottom])
@@ -63,7 +80,23 @@ struct ContentView: View {
     
     func loadImage() {
         guard let inputImage = inputImage else { return }
-        image = Image(uiImage: inputImage)
+//        image = Image(uiImage: inputImage)
+        
+        filterIntensity = 0.5
+        
+        let beginImage = CIImage(image: inputImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
+    }
+    
+    func applyProcessing() {
+        currentFilter.intensity = Float(filterIntensity)
+        guard let outputImage = currentFilter.outputImage else { return }
+        
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgimg)
+            image = Image(uiImage: uiImage)
+        }
     }
 }
 
